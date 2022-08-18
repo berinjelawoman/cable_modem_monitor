38783be0fe8e9c3388da9993663b0a39c3c17361
Berin Jela <berinjelawoman@gmail.com>
2022-08-18 17:12:42 -0300

    Added backend websocket server

---
+++ backend/data_parser.py
@@ -0,0 +1,107 @@
+#%%
+import os
+import re
+import json
+import tarfile
+
+from functools import reduce
+from datetime import datetime
+
+from typing import *
+
+DATA_FOLDER = "/home/servidor/.bk"
+
+
+def _atoi(text):
+    return int(text) if text.isdigit() else text
+
+
+def _natural_keys(text):
+    """
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    https://stackoverflow.com/questions/5967500/how-to-correctly-sort-a-string-with-a-number-inside
+    """
+    return [ _atoi(c) for c in re.split(r'(\d+)', text) ]
+
+
+def merge_jsons(m_jsons: List[Dict[Any, Any]]) -> Dict[Any, Any]:
+    return reduce(lambda res, x: res | x, m_jsons, {})    
+
+
+def unix_to_date(timestamp: str) -> datetime:
+    """
+    Wrapper of datetime function to convert strings containing a 
+    unix timestamp to datetime format
+    """
+    try:
+        return datetime.fromtimestamp(int(timestamp))
+    except ValueError:
+        print(f"Invalid timestamp {timestamp}")
+
+
+
+def get_usage_data() -> Iterator[Dict[str, Dict[str, List[int]]]]:
+    """
+    Returns a generator to iterate over the available usage data
+    """
+    # sort files by creation order, represented by the name numbering
+    files =[f"{DATA_FOLDER}/{file}" 
+        for file in os.listdir(DATA_FOLDER) if file.endswith(".gz")]
+    files.sort(key=_natural_keys, reverse=True)
+
+    # load files from newest to older
+    for file in files:
+        with tarfile.open(file) as tar:
+            for m in tar.getmembers():
+                data = json.loads(tar.extractfile(m).read())
+
+                # we only cara about the "Us Bytes" and "Ds Bytes" keys
+                targets = ["Us Bytes", "Ds Bytes"]
+                data = {date: {key: [int(i) for i in items] 
+                    for key, items in data[date].items() if key in targets}
+                        for date in data}
+                
+                yield data
+
+
+
+def get_upto_nth_usage_data(n: int) -> Iterator[Dict[str, Dict[str, List[int]]]]:
+    """
+    Returns the usage data up to the nth day
+    """
+    usage_data = get_usage_data()
+    now = datetime.now()
+    for data in usage_data:
+        end_data = unix_to_date(list(data.keys())[-1])
+        # if the difference is bigger than 7 days, stop iterating
+        if (now - end_data).days >= n:
+            break
+        yield data
+
+
+
+def get_last_day_usage_data() -> Iterator[Dict[str, Dict[str, List[int]]]]:
+    """
+    Returns a generator to iterate over the last day available usage data
+    """
+    return get_upto_nth_usage_data(1)
+
+
+def get_last_week_usage_data() -> Iterator[Dict[str, Dict[str, List[int]]]]:
+    """
+    Returns a generator to iterate over the last week available usage data
+    """
+    return get_upto_nth_usage_data(7)
+
+
+
+if __name__ == "__main__":
+    for i, data in enumerate(get_last_day_usage_data()):
+        print(i)
+    
+    data = get_usage_data()
+    from sys import getsizeof
+    d = next(data)
+    print(getsizeof(d))
+# %%
---
+++ backend/serve_previous_data.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import json
+import asyncio
+import websockets
+import websockets.server
+
+import data_parser as dp
+
+
+async def handler(websocket: websockets.server.WebSocketServerProtocol):
+    """
+    Handles data connection and dispatches necessary data
+    """
+    print("Connected")
+    # Receive and parse the amount of days wanted
+    message = await websocket.recv()
+    print(message)
+    json_message = json.loads(message)
+    days = json_message["days"]
+    
+    for i, data in enumerate(dp.get_upto_nth_usage_data(days)):
+        to_send = {}
+        to_send["id"] = i
+        to_send["data"] = data
+        await websocket.send(json.dumps(to_send, separators=(",", ":")))
+        
+
+    # send empty data with negative IP to mark message end
+    to_send = {}
+    to_send["id"] = -1
+    to_send["data"] = []
+    await websocket.send(json.dumps(to_send, separators=(",", ":")))
+
+
+async def main():
+    async with websockets.serve(handler, "", 8001):
+        await asyncio.Future()  # run forever
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
